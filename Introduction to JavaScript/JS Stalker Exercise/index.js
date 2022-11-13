e725d17aec29aa649abe67c366eced5024cf4cd6
HQPE <65927735+HQPE@users.noreply.github.com>
2022-11-14 02:32:00 +0300

    Add files via upload

---
+++ Introduction to JavaScript/JS Stalker Exercise/index.js
@@ -0,0 +1,8 @@
+var FirstName = prompt("What is your first name");
+var LastName = prompt("What is your last name");
+var Age = prompt("What is your age");
+
+var FullName = FirstName + " " + LastName;
+
+console.log("Your full name is " + FullName + ".");
+console.log("You are " + Age + " years old");
