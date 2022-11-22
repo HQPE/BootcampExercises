9e300c259b7e5d35a340492e01b9ad05d6697347
HQPE <65927735+HQPE@users.noreply.github.com>
2022-11-22 19:35:32 +0300

    Add files via upload

---
+++ Javascript Basics: Control Flow/For Loops Problem Set/index.js
@@ -0,0 +1,31 @@
+console.log("Print all integers from -10 to 19")
+
+for(var i = -10; i <= 19; i++){
+    console.log(i)
+}
+
+console.log("Print only integers from 10 to 40 which are even")
+
+for(var i = 10; i <=40; i++){
+    if(i %2 === 0){
+    console.log(i)
+    }
+}
+
+console.log("Print only integers from 300 to 333 which are odd")
+
+for(var i = 300; i <=333; i++){
+    if(i %2 !== 0){
+    console.log(i)
+    }
+}
+
+console.log("Print only integers from 300 to 333 which are odd")
+
+for(var i = 5; i <=50; i++){
+    if(i %3 === 0 && i %5 === 0){
+    console.log(i)
+    }
+}
+
+
